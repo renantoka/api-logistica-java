1037b7d72a8b4aca8b0cbd3342dc3a0ab51548c2
Renan Tokashiki <renantoka@icloud.com>
2021-08-12 16:07:34 -0400

    feat: criação do controller e model de cliente

---
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog.domain.model;
+
+import lombok.Data;
+
+
+@Data
+public class Cliente {
+
+		private Long id;
+		private String nome;
+		private String email;
+		private String telefone;
+}
