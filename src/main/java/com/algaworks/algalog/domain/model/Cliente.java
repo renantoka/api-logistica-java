4462ed33d0c26d0b95d73c77c0e6502ece083a15
Renan Tokashiki <renantoka@icloud.com>
2021-08-12 17:25:57 -0400

    feat: criada configuração para utilização do Jakarta EE

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,13 +1,26 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
+@Entity
 public class Cliente {
-
+		
+		@EqualsAndHashCode.Include
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
+		
 		private String nome;
 		private String email;
 		private String telefone;
+		
+		
 }
