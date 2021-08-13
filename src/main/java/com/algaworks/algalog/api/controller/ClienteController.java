a404849bd9d6a82c0aef8b50ce8c3f503dd518bd
Renan Tokashiki <renantoka@icloud.com>
2021-08-13 13:23:36 -0400

    feat: criado o repository e adicionado o CRUD

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,24 +1,83 @@
 package com.algaworks.algalog.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-		@PersistenceContext
-		private EntityManager manager;
+		@Autowired
+		private ClienteRepository clienteRepository;
 		
-		@GetMapping("/clientes")
-		public List<Cliente> listar() {
-			return manager.createQuery("from Cliente", Cliente.class)
-					.getResultList();
+		@GetMapping("/")
+		public List<Cliente> listarClientes() {
+			return clienteRepository.findAll();
+		}
+		
+		@GetMapping("/{clienteId}")
+		public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long clienteId) {
+			return clienteRepository.findById(clienteId)
+					.map(ResponseEntity::ok)
+					.orElse(ResponseEntity.notFound().build());
+		
+// 		outra forma de fazer utilizando condicional
+//			Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//			
+//			if (cliente.isPresent()) {
+//				return ResponseEntity.ok(cliente.get());
+//			}
+//			
+//			return ResponseEntity.notFound().build();
+		}
+		
+		@PostMapping
+		@ResponseStatus(HttpStatus.CREATED)
+		public Cliente cadastrarCliente(@RequestBody Cliente cliente) {
+			return clienteRepository.save(cliente);
+		}
+		
+		@PutMapping("/{clienteId}")
+		public ResponseEntity<Cliente> alterarCliente(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+			if (!clienteRepository.existsById(clienteId)) {
+				
+			return ResponseEntity.notFound().build();
+			}
+			
+			cliente.setId(clienteId);
+			cliente = clienteRepository.save(cliente);
+			
+			return ResponseEntity.ok(cliente);
+		}
+		
+		@DeleteMapping("/{clienteId}")
+		public ResponseEntity<Cliente> removerCliente(@PathVariable Long clienteId) {
+			if (!clienteRepository.existsById(clienteId)) {
+				
+				return ResponseEntity.notFound().build();
+				}
+				
+				clienteRepository.deleteById(clienteId);
+				
+				return ResponseEntity.noContent().build();
 		}
 }
