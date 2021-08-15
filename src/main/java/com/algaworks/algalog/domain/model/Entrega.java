eb55d1e20891960e2fbe47266d00673fe9e93642
Renan Tokashiki <renantoka@icloud.com>
2021-08-15 18:25:22 -0400

    feat: Criado solicitação de entrega e validação em cascata com validation groups

---
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -0,0 +1,57 @@
+package com.algaworks.algalog.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+
+import com.algaworks.algalog.domain.ValidationGroups;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Entrega {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
+	@ManyToOne
+	private Cliente cliente;
+	
+	@Embedded
+	private Destinatario destinatario;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	@Enumerated(EnumType.STRING)
+	private StatusEntrega status;
+	
+	@NotNull
+	private BigDecimal taxa;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	private LocalDateTime dataPedido;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	private LocalDateTime dataFinalizado;
+}
---
+++ src/main/java/com/algaworks/algalog/domain/model/Ocorrencia.java
@@ -0,0 +1,8 @@
+package com.algaworks.algalog.domain.model;
+
+import java.util.Date;
+
+public class Ocorrencia {
+	private String descricao;
+	private Date dataRegistro;
+}
